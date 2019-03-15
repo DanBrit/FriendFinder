4a1b7bf937dcb70751d7c2938b2a8eec8f7e210c
dmar <dan.britain.marshall@gmail.com>
2019-03-15 18:54:51 -0500

    working commit

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+// Variable to hold the array of friends objects data
+var friends = [
+
+    { "name": "Ahmed", "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg", "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"] }, 
+    { "name": "Daniel Craig", "photo": "https://m.media-amazon.com/images/M/MV5BMjEzMjk4NDU4MF5BMl5BanBnXkFtZTcwMDMyNjQzMg@@._V1_.jpg", "scores": ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"] }, 
+    { "name": "Robert Downey Jr.", "photo": "https://m.media-amazon.com/images/M/MV5BNzg1MTUyNDYxOF5BMl5BanBnXkFtZTgwNTQ4MTE2MjE@._V1_SY1000_CR0,0,664,1000_AL_.jpg", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
+    { "name": "Woody Harrelson", "photo": "https://m.media-amazon.com/images/M/MV5BMTU3NDc2ODc4MF5BMl5BanBnXkFtZTcwODk2MzAyMg@@._V1_UY317_CR1,0,214,317_AL_.jpg", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
+    { "name": "Harrison Ford", "photo": "https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] }, 
+    { "name": "Jordan Biason", "photo": "https://m.media-amazon.com/images/M/MV5BMTY4Mjg0NjIxOV5BMl5BanBnXkFtZTcwMTM2NTI3MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
+
+// Export the data
+module.exports = friends;
