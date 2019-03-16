f8b9c8a343b8dd202b2ec1f58a5fa0a9a1d65574
dmar <dan.britain.marshall@gmail.com>
2019-03-15 21:28:53 -0500

    fixed friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,8 +6,8 @@ var friends = [
     { "name": "Lisa Kudrow", "photo": "https://m.media-amazon.com/images/M/MV5BMTU5OTA0ODcxNl5BMl5BanBnXkFtZTcwMjE3NjQxMw@@._V1_UY317_CR8,0,214,317_AL_.jpg", "scores": ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"] }, 
     { "name": "Matt LeBlanc", "photo": "https://m.media-amazon.com/images/M/MV5BODQ0NTI0OTk0M15BMl5BanBnXkFtZTcwMDk2MDg5Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg", "scores": ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"] }, 
     { "name": "Matthew Perry", "photo": "https://m.media-amazon.com/images/M/MV5BMTMwODc5NjI3N15BMl5BanBnXkFtZTcwNDEyMTE3Mw@@._V1_UY317_CR17,0,214,317_AL_.jpg", "scores": ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"] },
-    { "name": "Paul Rudd", "photo": "https://m.media-amazon.com/images/M/MV5BMTY4NTEwNDg1MV5BMl5BanBnXkFtZTgwODMwMDA0ODE@._V1_UY317_CR20,0,214,317_AL_.jpg", "scores": ["5", "2", "3", "4", "1", "2", "3", "2", "4", "5"] };,
-    { "name": "Elliot Gould", "photo": "https://m.media-amazon.com/images/M/MV5BMjEzNzU0MTA0OV5BMl5BanBnXkFtZTcwODQ2NDM2Mw@@._V1_UY317_CR2,0,214,317_AL_.jpg", "scores": ["2", "3", "5", "2", "3", "2", "3", "2", "4", "5"] };, 
+    { "name": "Paul Rudd", "photo": "https://m.media-amazon.com/images/M/MV5BMTY4NTEwNDg1MV5BMl5BanBnXkFtZTgwODMwMDA0ODE@._V1_UY317_CR20,0,214,317_AL_.jpg", "scores": ["5", "2", "3", "4", "1", "2", "3", "2", "4", "5"] },
+    { "name": "Elliot Gould", "photo": "https://m.media-amazon.com/images/M/MV5BMjEzNzU0MTA0OV5BMl5BanBnXkFtZTcwODQ2NDM2Mw@@._V1_UY317_CR2,0,214,317_AL_.jpg", "scores": ["2", "3", "5", "2", "3", "2", "3", "2", "4", "5"] }, 
     { "name": "David Schwimmer", "photo": "https://m.media-amazon.com/images/M/MV5BMTQ2Mjg5ODIzNF5BMl5BanBnXkFtZTgwNjIwMjI0ODE@._V1_UX214_CR0,0,214,317_AL_.jpg", "scores": ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"] }];
     
 
